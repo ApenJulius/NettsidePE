00c0a506d0c868dfa256afd06966fe30d42d2f2f
ApenJulius <34554076+ApenJulius@users.noreply.github.com>
2024-05-13 19:40:52 +0200

    SquareContainer now looks somewhat similar

--- frontend/src/components/SquareContainer/SquareContainer.tsx
+++ frontend/src/components/SquareContainer/SquareContainer.tsx
@@ -1,22 +1,35 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, ElementType } from "react";
 import FacebookIcon from '@mui/icons-material/Facebook';
-
+import EastSharpIcon from '@mui/icons-material/EastSharp';
 interface SquareContainerProps {
-    logo: any; // update the type to logo type
+    Logo: ElementType; // update the type to logo type
     title: string;
     description: string;
     readMore:boolean;
 }
 
-const SquareContainer: React.FC<SquareContainerProps> = ({logo, title, description, readMore}) => {
+const SquareContainer: React.FC<SquareContainerProps> = ({Logo, title, description, readMore}) => {
 
     return (
-       <div className="">
-            {logo}
-            <h3>{title}</h3>
-            <p>{description}</p>
-            {readMore && <button>Read More</button>}
-       </div>
+        <div className="border-1 w-80 h-80 flex flex-col justify-between p-7 light-shadow shadow-xl rounded-lg">
+            <div className="self-start">
+                <Logo style={{ fontSize: 40, backgroundColor: '#000', borderRadius: '50%', padding: '10px', color: '#fff' }} />
+            </div>
+            <div className="overflow-hidden">
+                <h3 className="break-words overflow-ellipsis overflow-hidden font-bold mb-3">{title}</h3>
+                <p className="overflow-ellipsis overflow-hidden text-xs text-gray-500">{description}</p>
+            </div>
+            <div className="">
+                {readMore && 
+                
+                <button className="flex justify-between items-center w-full text-blue-500">
+                    <span>Read More</span>
+                    <EastSharpIcon style={{ fontSize: 20, color:"#3b82f6" }} />
+                </button>
+                }
+
+            </div>
+        </div>
     );
 }
 
