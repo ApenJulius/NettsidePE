4e5417e149116c3122053be657754bc5fd29b877
ApenJulius <34554076+ApenJulius@users.noreply.github.com>
2024-05-13 14:39:13 +0200

    Basic footer added. Incorrect test

---
+++ frontend/src/components/Footer/Footer.tsx
@@ -0,0 +1,33 @@
+import React, { useEffect, useState } from "react";
+import FacebookIcon from '@mui/icons-material/Facebook';
+import MailIcon from '@mui/icons-material/Mail';
+const Footer: React.FC = () => {
+
+    return (
+        <footer className="flex flex-direction-row width-100 justify-around items-center h-20">
+            <div>
+                Logo
+            </div>
+            <ul>
+                <li>About Us</li>
+                <li>us</li>
+                <li>us</li>
+                <li>us</li>
+            </ul>
+            <ul>
+                <li>Contact Us</li>
+                <li>Address</li>
+                <li>Email</li>
+                <li>S</li>
+            </ul>
+            <ul>
+                <li>Follow Us:</li>
+                <li>Facebook</li>
+                <li>Facebook</li>
+                <li>Facebook</li>
+            </ul>
+        </footer>
+    );
+}
+
+export default Footer;
