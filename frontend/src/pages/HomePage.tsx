a6fd01062aadcf0312de4f77626d837f20b57f07
ApenJulius <34554076+ApenJulius@users.noreply.github.com>
2024-05-13 12:33:39 +0200

    Created default pages

---
+++ frontend/src/pages/HomePage.tsx
@@ -0,0 +1,14 @@
+import React, { useEffect, useState } from "react";
+
+
+
+const HomePage: React.FC = () => {
+
+    return (
+        <div>
+            <h1>Home Page</h1>
+        </div>
+    );
+}
+
+export default HomePage;
---
+++ frontend/src/pages/NoPage.tsx
@@ -0,0 +1,14 @@
+import React, { useEffect, useState } from "react";
+
+
+
+const NoPage: React.FC = () => {
+
+    return (
+        <div>
+            <h1>No Page</h1>
+        </div>
+    );
+}
+
+export default NoPage;
