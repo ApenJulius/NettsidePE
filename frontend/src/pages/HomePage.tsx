85ca7ce0c0bda28495ac4e2c14ed279331d7f759
ApenJulius <34554076+ApenJulius@users.noreply.github.com>
2024-05-13 14:39:56 +0200

    Updated HomePage to use new components

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,12 +1,23 @@
 import React, { useEffect, useState } from "react";
-
+import Header from "../components/Header/Header";
+import Footer from "../components/Footer/Footer";
+import SquareContainer from "../components/SquareContainer/SquareContainer";
 
 
 const HomePage: React.FC = () => {
 
     return (
         <div>
-            <h1>Home Page</h1>
+            <Header />
+            <div className="justify-center items-center flex flex-col">
+                <h1 className="text-center text-8xl p-5 ">Cyber Security Services</h1>
+                <p className="text-center break-normal p-10 w-1/2 ">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab nulla eveniet necessitatibus laudantium, nam aut quaerat ipsum eum adipisci, dolorem ex doloribus incidunt molestias quia deleniti veritatis non rem fugit.</p>
+                <button className="bg-gradient-to-r from-[#034a22] to-[#f63238] p-2 rounded-lg">Get Started Now</button>
+            </div>
+
+            <SquareContainer />
+
+            <Footer />
         </div>
     );
 }
