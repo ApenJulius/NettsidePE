0738da8620ae6f3f2be73ff32c636d144c327f94
ApenJulius <34554076+ApenJulius@users.noreply.github.com>
2024-05-14 15:51:08 +0200

    Added map to the page

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -3,6 +3,10 @@ import Header from "../components/Header/Header";
 import Footer from "../components/Footer/Footer";
 import SquareContainer from "../components/SquareContainer/SquareContainer";
 import CloudDownloadIcon from '@mui/icons-material/CloudDownload';
+import EmployeeCard from "../components/EmployeeCard/EmployeeCard";
+import man1 from '../assets/man1.jpg';
+import Map from "../components/Map/Map";
+
 
 const HomePage: React.FC = () => {
 
@@ -27,9 +31,19 @@ const HomePage: React.FC = () => {
             </div>  
 
 
+            <div className="justify-center items-center flex flex-col">
+                <h2 className="text-center text-5xl p-5 font-bold">Our Team</h2>
+                <p className="text-center break-normal p-10 w-1/2 ">The process of evaluating, identifying and managing risks associated with a business, and ensuring compliance with relevant regulations and standards</p>
+            </div>  
 
+            <div className="flex justify-between mt-20 mb-20">
+                <EmployeeCard image={man1} title="Managed Detection and Response" description="We take #1 breach prevention software, CrowdStrike, and couple it with 100% U.S. threat hunting and response services 24x7x365" link=""/>
+                <EmployeeCard image={man1} title="Managed Detection and Response" description="We take #1 breacaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaah prevention software, CrowdStrike, and couple it with 100% U.S. threat hunting and response services 24x7x365" link="Asd"/>
+                <EmployeeCard image={man1} title="Managed Detection and Response" description="We take #1 breach prevention software, CrowdStrike, and couple it with 100% U.S. threat hunting and response services 24x7x365" link=""/>
+                <EmployeeCard image={man1} title="Managed Detection and Response" description="We take #1 breach prevention software, CrowdStrike, and couple it with 100% U.S. threat hunting and response services 24x7x365" link=""/>
+            </div>
 
-
+            <Map />
 
             <Footer />
         </div>
