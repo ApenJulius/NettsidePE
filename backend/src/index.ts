1821a64f6f544a5e57ed9827f1786d6ba740534d
ApenJulius <34554076+ApenJulius@users.noreply.github.com>
2024-05-15 15:00:08 +0200

    Database handling added

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -11,6 +11,7 @@ const AppDataSource = new DataSource({
     type: "sqlite",
     database: "./db.sqlite",
     entities: ["src/models/*.ts", "./models/*.js"],
+    synchronize: true,
 })
 
 AppDataSource.initialize()
@@ -27,13 +28,34 @@ AppDataSource.initialize()
 
 const app: Express = express();
 const port = process.env.PORT || 3000;
-
-app.get("/contact-forms", (req: Request, res: Response) => {
-  res.send("Express + TypeScript Server");
+app.use(express.json());
+app.get("/contact-forms", async (req: Request, res: Response) => {
+  try {
+    const contactFormRepository = AppDataSource.getRepository("ContactForm");
+    const allContactForms = await contactFormRepository.find();
+    res.json(allContactForms);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Error occurred while fetching data");
+  }
 });
 
-app.post("/contact-forms", (req: Request, res: Response) => {
-  res.send("Express + TypeScript Server");
+app.post("/contact-forms/submit", async (req: Request, res: Response) => {
+
+
+  try {
+    const contactFormRepository = AppDataSource.getRepository("ContactForm");
+    const newContactForm = contactFormRepository.create({
+      name: req.body.name.toString(),
+      phone: req.body.phone.toString(),
+      message: req.body.message.toString(),
+    });
+    await contactFormRepository.save(newContactForm);
+    res.json({ message: "Contact form created successfully" });
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Error occurred while creating data");
+  }
 });
 
 
