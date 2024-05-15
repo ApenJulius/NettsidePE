6caf546a1aab836b5a2b2ab09087ddb59c9e0364
ApenJulius <34554076+ApenJulius@users.noreply.github.com>
2024-05-15 12:24:02 +0200

    Contact form is added to main page

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -7,9 +7,15 @@ import EmployeeCard from "../components/EmployeeCard/EmployeeCard";
 import man1 from '../assets/man1.jpg';
 import Map from "../components/Map/Map";
 import Cookie from "../components/Cookie/Cookie";
+import ContactButton from "../components/ContactButton/ContactButton";
 
 
 const HomePage: React.FC = () => {
+    
+    const handleClick = () => {
+        console.log("Get Started Now button clicked");
+    }
+
 
     return (
         <div>
@@ -18,7 +24,7 @@ const HomePage: React.FC = () => {
             <div className="justify-center items-center flex flex-col">
                 <h1 className="text-center text-8xl p-5 font-bold ">Cyber Security Services</h1>
                 <p className="text-center break-normal p-10 w-1/2 ">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab nulla eveniet necessitatibus laudantium, nam aut quaerat ipsum eum adipisci, dolorem ex doloribus incidunt molestias quia deleniti veritatis non rem fugit.</p>
-                <button className="bg-gradient-to-b from-[#034a22] to-[#095611] p-5 rounded-lg text-xl">Get Started Now</button>
+                <ContactButton />
             </div>
             <div className="flex justify-between mt-20 mb-20">
                 <SquareContainer Logo={CloudDownloadIcon} title="Managed Detection and Response" description="We take #1 breach prevention software, CrowdStrike, and couple it with 100% U.S. threat hunting and response services 24x7x365" readMore={true}/>
